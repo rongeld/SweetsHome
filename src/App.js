4dbd8fd05c3080ecd5190775cae77cba47e2a3fe
Andrii Zakrevskyi <andrew.zakrevskiy@gmail.com>
2020-02-03 19:03:09 +0100

    collection-preview

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from './pages/homePage/HomePage';
+import ShopPage from './pages/shop/ShopPage';
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={() => <HomePage />} />
-        <Route path='/cakes' exact component={() => <HomePage />} />
+        <Route path='/shop' exact component={() => <ShopPage />} />
       </Switch>
     </BrowserRouter>
   )
